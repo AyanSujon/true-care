06bb83e904bf124478a03a7beebf8fac8e039aa4
Ayan <sujon.seo.bd@gmail.com>
2026-01-18 17:15:42 +0600

    fixing error from this route: ./actions/server/regions.ts:14:23

--- actions/server/regions.ts
+++ actions/server/regions.ts
@@ -2,16 +2,74 @@
 
 
 
+// "use server";
+
+// import { collections, dbConnect } from "@/lib/dbConnect";
+// import { ObjectId } from "mongodb";
+
+// export const getRegions = async () => {
+//   const regionsCollection = await dbConnect(collections.REGIONS);
+//   const regions = await regionsCollection.find({}).toArray();
+
+//   return regions.map((region) => ({
+//     ...region,
+//     _id: region._id.toString(),
+//   }));
+// };
+
+// export const getSingleRegion = async (id: string) => {
+//   if (!id || !ObjectId.isValid(id)) {  // ← Fixed: is_valid (lowercase)
+//     return {}; // or null / throw new Error("Invalid ID")
+//   }
+
+//   const query = { _id: new ObjectId(id) };
+//   const regionsCollection = await dbConnect(collections.REGIONS);
+//   const region = await regionsCollection.findOne(query);
+
+//   if (!region) {
+//     return {}; // or null
+//   }
+
+//   return {
+//     ...region,
+//     _id: region._id.toString(),
+//   };
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "use server";
 
 import { collections, dbConnect } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 
+interface Region {
+  _id: ObjectId;
+  region: string;
+  district: string;
+  // add other fields if needed
+}
+
 export const getRegions = async () => {
   const regionsCollection = await dbConnect(collections.REGIONS);
   const regions = await regionsCollection.find({}).toArray();
 
-  return regions.map((region) => ({
+  return regions.map((region: Region) => ({
     ...region,
     _id: region._id.toString(),
   }));
@@ -35,3 +93,6 @@ export const getSingleRegion = async (id: string) => {
     _id: region._id.toString(),
   };
 };
+
+
+
